1de41cefc8db05fe17aee7fe85e3b6dbd0b5a914
AntonChislov <antonchislov6642@gmail.com>
2023-01-31 22:49:57 +0300

    dz 9 completed fix

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -50,11 +50,11 @@ function Clock() {
     });
 
     const stringTime = hourMinute.format(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = month.format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = dateSpan.format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = weekday.format(date) || <br/> // пишут студенты
-    const stringMonth = dateSpan.format(date) || <br/> // пишут студенты
+    const stringMonth = month.format(date) || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
